1eef4a3fcc8619743a50a19d67cd2cd8df3a219f
henriqueortiz975 <henriqueortiz3090@gmail.com>
2026-03-13 16:31:21 -0300

    finalizado

---
+++ Exercicio9_Personagens_de_Jogo/src/exercicio9_personagens_de_jogo/Paladino.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio9_personagens_de_jogo;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class Paladino {
+    
+}
